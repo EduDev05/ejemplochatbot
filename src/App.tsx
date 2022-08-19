98112059b76a39cb6ca78b349e7ce4482becdc8b
unknown <eduardo.garcia@isita.com.mx>
2022-08-19 12:55:30 -0500

    cambios para chatbot ultima vesion

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+// import './App.css';
+import {Chatbot} from './components/Chatbot';
+// import './styles/index.css';
+import './styles/estilosg.css';
+
+function App() {
+  return (
+    <div >
+<Chatbot/>
+    </div>
+  );
+}
+
+export default App;
