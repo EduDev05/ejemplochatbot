4173714a8e5c15fd91d6e445ebc640e7c85afb87
unknown <eduardo.garcia@isita.com.mx>
2022-09-02 16:50:20 -0500

    segundo intento

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,17 @@ import {Chatbot} from './components/Chatbot';
 import './styles/estilosg.css';
 
 function App() {
+  window.addEventListener('message', (event) => {
+    // Reject all messages except ones from https://parent.example.com
+    if (event.origin !== 'https://pruebachat--tecmonterreymxqa.myvtex.com/') return;
+  
+    // Do a DOM manipulation on https://video.example.com.
+  
+    // Send a success message to https://parent.example.com
+    event.source.postMessage('succeeded', event.origin);
+  });
+
+
   //  document.domain="pruebachat--tecmonterreymxqa.myvtex.com";
   console.log("domino", document.domain);
   return (
