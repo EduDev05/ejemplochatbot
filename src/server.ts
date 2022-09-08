4e59392a82ccadad7c05eccbc98df497658214a9
unknown <eduardo.garcia@isita.com.mx>
2022-09-08 12:54:01 -0500

    ultimo cambios

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,14 @@ import express from 'express';
 import cors from 'cors';
 
 const app = express()
-const port = process.env.PORT || 300;
+// const port = process.env.PORT || 300;
+
+// const corsOptions = {
+//   origin: 'http://localhost:3000', 
+//   credentials: true, 
+//   optionsSuccessStatus: 200 
+// }
+//app.use(cors(corsOptions));
 
 app.use(cors());
 
