0d3d202c89b99e796323c9f2f0d28f166b422f80
Eduardo García García <eduardo.garcia@isita.com.mx>
2023-01-11 17:10:11 -0600

    intento 5:09 para no dejar XD

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,10 @@ function App() {
       <Helmet>
         <meta http-equiv="Content-Security-Policy"  content="frame-ancestors *" />
         <meta http-equiv="Content-Security-Policy"  content="frame-src *" />
+        <meta http-equiv="Content-Security-Policy"  content="frame-ancestors https://tecmonterreymxqa.myvtex.com" />
+        <meta http-equiv="Content-Security-Policy"  content="frame-src https://tecmonterreymxqa.myvtex.com" />
+        <meta http-equiv="Content-Security-Policy"  content="frame-ancestors https://testchatbot--tecmonterreymxqa.myvtex.com/" />
+        <meta http-equiv="Content-Security-Policy"  content="frame-src https://testchatbot--tecmonterreymxqa.myvtex.com/" />
         Content-Security-Policy: frame-src *; frame-ancestors *;
       </Helmet>
     <Chatbot/>
