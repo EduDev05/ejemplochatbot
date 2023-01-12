63ed52993c5abddb30ff0e004a5c3152b3f4eafb
Eduardo García García <eduardo.garcia@isita.com.mx>
2023-01-11 18:18:17 -0600

    cambios 'self' 6:17

--- src/App.tsx
+++ src/App.tsx
@@ -9,12 +9,13 @@ function App() {
   return (
     <div >
       <Helmet>
-        <meta http-equiv="Content-Security-Policy"  content="frame-ancestors *" />
+        <meta http-equiv="Content-Security-Policy"  content="frame-ancestors 'self' *" />
         <meta http-equiv="Content-Security-Policy"  content="frame-src *" />
-        <meta http-equiv="Content-Security-Policy"  content="frame-ancestors https://tecmonterreymxqa.myvtex.com" />
+        <meta http-equiv="Content-Security-Policy"  content="frame-ancestors 'self' https://tecmonterreymxqa.myvtex.com" />
         <meta http-equiv="Content-Security-Policy"  content="frame-src https://tecmonterreymxqa.myvtex.com" />
-        <meta http-equiv="Content-Security-Policy"  content="frame-ancestors https://testchatbot--tecmonterreymxqa.myvtex.com/" />
+        <meta http-equiv="Content-Security-Policy"  content="frame-ancestors 'self' https://testchatbot--tecmonterreymxqa.myvtex.com/" />
         <meta http-equiv="Content-Security-Policy"  content="frame-src https://testchatbot--tecmonterreymxqa.myvtex.com/" />
+        <meta http-equiv="Content-Security-Policy"  content="frame-ancestors 'self' https://mycourses.w3schools.com" />
         Content-Security-Policy: frame-src *; frame-ancestors *;
       </Helmet>
     <Chatbot/>
